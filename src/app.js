8d44b11c7a7884363401170e068efa14e0c36b5a
maoc86 <soymanuweb@gmail.com>
2022-10-12 21:40:16 +0000

    mi primer version

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird", "samuel"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  document.querySelector("#excuse").innerHTML = `
+  ${who[getRandomInt(who.length)]}  
+  ${what[getRandomInt(what.length)]} 
+  ${when[getRandomInt(when.length)]} `;
 };
