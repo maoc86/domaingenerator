213d3988b0d50e0f6062488271a1448fad167874
maoc86 <soymanuweb@gmail.com>
2022-10-13 00:29:19 +0000

    Primera Version

--- src/app.js
+++ src/app.js
@@ -5,22 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+//write your code here
 
 window.onload = function() {
-  let who = ["the dog", "my granma", "his turtle", "my bird", "samuel"];
-  let what = ["eat", "pissed", "crushed", "broked"];
-  let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  document.querySelector("#excuse").innerHTML = `
-  ${who[getRandomInt(who.length)]}  
-  ${what[getRandomInt(what.length)]} 
-  ${when[getRandomInt(when.length)]} `;
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const ext = [".com", ".es"];
+
+  let domain = [];
+
+  pronoun.forEach(pron => {
+    adj.forEach(ad => {
+      noun.forEach(nou => {
+        ext.forEach(ex => {
+          if (ex == ".com") domain.push(`${pron}${ad}${nou}${ex}`);
+        });
+      });
+    });
+  });
+
+  document.querySelector("#domain").innerHTML = domain
+    .map(x => `<li>${x}</li>`)
+    .join("");
 };
